8a4b42a389993ddbf07cfd58bf1cee052b1237aa
Prathmesh Vhatkar <109974838+Murphy120814@users.noreply.github.com>
2024-04-19 12:47:23 +0530

    Update AuthenticationForm.jsx

--- src/components/AuthenticationForm.jsx
+++ src/components/AuthenticationForm.jsx
@@ -123,10 +123,9 @@ function AuthenticationForm() {
             className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800  disabled:bg-black disabled:opacity-20 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto">
             Log In
           </button>
-          {/* <span className="font-semibold text-red-600">
-            {" "}
-            {showErrorMessage}
-          </span> */}
+          <p className="font-semibold text-black dark:text-white">
+           For admin usage use Email: <span className="text-red-500">prathm@frontendmeta.dev</span> and password as <span className="text-red-500">Admin@12</span> 
+          </p>
         </Form>
       )}
     </Formik>
